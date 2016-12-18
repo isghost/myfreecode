8acf02f850f33b5a8fd7a73f57583d7ff2f651f2
ccy <810278677@qq.com>
2016-12-18 14:47:48 +0800

    [upd] 图片链接改成github

--- PersonalPortfolio/main.js
+++ PersonalPortfolio/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	$(".portfolio .item").map(function(count){
-		$(this).css("background-image", "url(images/cover"+(count + 1)+ ".jpg)");
+		$(this).css("background-image", "url(https://github.com/isghost/myfreecode/raw/master/PersonalPortfolio/images/cover"+(count + 1)+ ".jpg)");
 	});
+
+
 });
