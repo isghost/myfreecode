51f2b80d2772a4f549db53430ea5f0d1f944f04b
ccy <810278677@qq.com>
2016-12-31 17:14:31 +0800

    [upd] 添加异常捕获

--- ServerAll/main.js
+++ ServerAll/main.js
@@ -3,12 +3,21 @@ var app = express();
 var quotesRouter = require("./randomQuotesRouter.js");
 var localWeather = require('./localWeatherSign.js');
 
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ', err);
+});
+
 app.use('/api/randomQuotes', quotesRouter);
 
 app.use('/api/localWeather', localWeather);
 
 app.get('/', function (req, res) {
   res.send('这里暂未使用，联系方式810278677@qq.com');
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('可能某些地方出了问题!');
 });
 
 var server = app.listen(3000, function () {
