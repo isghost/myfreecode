d973020b68a6e607f8d80f9991e8b89dc9251d3d
ccy <810278677@qq.com>
2017-02-04 12:58:31 +0800

    [upd] 完成番茄钟

--- PomodoroClock/main.js
+++ PomodoroClock/main.js
@@ -1,14 +1,12 @@
 // svg  知识 https://developer.mozilla.org/zh-CN/docs/Web/SVG/Tutorial/Paths
+let curStatus = 2; // 1 break 2 session
+let countDown = 25 * 60;
+let curCountDownTotal = 25 * 60;
+let isPlaying = false;
 $(document).ready(function(){
-	setCircle(0.6);
-	let percent = 0;
-	setInterval(function(){
-		percent = percent + 0.01;
-		if(percent > 1){
-			percent = 0;
-		}
-		//setCircle(percent);
-	}, 30);
+	addAdjustCb();
+	setCircle(0.0);
+	startCountDown();
 });
 
 function setCircle(percent){
@@ -27,6 +25,84 @@ function setCircle(percent){
 	}
 	var pathStr = "M" + beginX + " " + beginY + " A " + radius + " " + radius + ", 0 ," + arcFlag + ", " +
 		" 0, " + endX + ", " + endY + " Z";
-	console.log(pathStr); 
+	// console.log(pathStr); 
 	$("#bgCircle").attr("d",pathStr);
+}
+
+function addAdjustCb(){
+	let adjustType = [1, 1, 2, 2]; // 1 break 2 session
+	let value = [-1, 1, -1, 1]; // minus 1 or add 1
+	let select = [".break .minus", ".break .add", ".session .minus", ".session .add"];
+	for(let i = 0;i < select.length; i++){
+		$(select[i]).click(function(){
+			if(isPlaying){
+				return ;
+			}
+			adjustTime(adjustType[i], value[i]);
+		});
+	}
+
+	$(".showTime").click(function(){
+		isPlaying = !isPlaying;
+	});
+}
+function adjustTime(adjustType, value){
+	let select = [".break .breakTime", ".session .breakTime"];
+	let breakTimeEle = $(select[adjustType -1 ]);
+	let countDownTotal = parseInt(breakTimeEle.text());
+	countDownTotal += value;
+	if(countDownTotal < 0){
+		return ;
+	}
+	breakTimeEle.text(countDownTotal);
+	if(adjustType == curStatus){
+		countDown = countDownTotal * 60;
+		$(".remainTime").text(formatTime(countDown));
+		curCountDownTotal = countDown;
+	}
+}
+
+function formatTime(time){
+	let min = Math.floor(time / 60);
+	let sec = time % 60;
+	if(sec < 10){
+		sec = "0" + sec;
+	}
+	return min + ":" + sec;
+}
+
+function startCountDown(){
+	setInterval(function(){
+		if(!isPlaying){
+			return ;
+		}
+		countDown = countDown - 1;
+		if(countDown < 0){
+			curStatus = !(curStatus - 1) + 1;
+			reset();
+			return ;
+		}
+		percent = (curCountDownTotal - countDown) / curCountDownTotal;
+		if(percent > 1){
+			percent = 0;
+		}
+		setCircle(percent);
+		$(".remainTime").text(formatTime(countDown));
+	}, 1000);
+}
+
+function reset(){
+	if(curStatus == 1){
+		$("#bgCircle").attr("fill", "orange");
+		$(".status").text("break");
+		countDown = parseInt($(".break .breakTime").text()) * 60;
+		curCountDownTotal = countDown;
+	}
+	else if(curStatus == 2){
+		$("#bgCircle").attr("fill", "blue");
+		$(".status").text("session");
+		countDown = parseInt($(".session .breakTime").text()) * 60;
+		curCountDownTotal = countDown;
+	}
+	setCircle(0.0);
 }
